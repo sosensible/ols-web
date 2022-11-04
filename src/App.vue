132296f0d6dc9664e8e7ecbf8d59d1c60a5ae82e
John Farrar <johnfarrar@sosapps.com>
2022-11-04 17:56:18 +0000

    Completed setup files. Added Router, Pinia, Supabase

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld
+    msg="Hello Vue 3 + TypeScript + Vite + Vue-Router + Pinia + Supabase"
+  />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-})
+    HelloWorld,
+  },
+});
 </script>
 
 <style>
